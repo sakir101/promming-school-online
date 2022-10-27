261debdf97142eee3c60d96fd18bd90cd7e813c1
sakir101 <sakirhossainf99@gmail.com>
2022-10-27 07:50:30 +0600

    Changes

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import MyCourses from '../MyCourses/MyCourses';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Courses = () => {
     return (
-        <div>
-            <h1>This is cousrses</h1>
-        </div>
+       <Container>
+            <Row>
+                <Col lg='3'>
+                    <Sidebar></Sidebar>
+                </Col>
+                <Col lg='9'>
+                    <MyCourses></MyCourses>
+                </Col>
+            </Row>
+       </Container>
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='bg-img'>
+            
         </div>
     );
 };
