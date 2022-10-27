002b8c99924e5a19d5679b782fdb1ce189690dc0
sakir101 <sakirhossainf99@gmail.com>
2022-10-27 10:08:35 +0600

    sidebar added

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom'
 const Sidebar = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect( () =>{
+        fetch('http://localhost:5000/category')
+        .then( res => res.json())
+        .then(data => setCategories(data));
+    }, [])
+    console.log(categories);
     return (
         <div>
-            <h1>Sidebar</h1>
+            <h3>Total Courses: {categories.length}</h3>
+            {
+                categories.map(category => <p key={category.id}>
+                    <Link to={`/category/${category.id}`}>{category.name}</Link>
+                </p>)
+            }
         </div>
     );
 };
